cbcf3b488ddab5312571111ee93bee8a78614cb2
Yogesh Solanki <solankiyogesh3500@gmail.com>
2025-10-26 12:48:40 +0530

    Initial Commit

---
+++ MacRemoteControl/MacRemoteControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacRemoteControlApp.swift
+//  MacRemoteControl
+//
+//  Created by Yogesh Solanki on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MacRemoteControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
